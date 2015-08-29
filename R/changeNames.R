52b4c9c791bcb2211e19021580559143bda1949f
ingvarp <ipetrov@sfu.ca>
2015-08-29 11:42:08 -0700

    default field names changed

--- R/changeNames.R
+++ R/changeNames.R
@@ -8,12 +8,12 @@
 #' 
 #' @return New column names of dataframe.
 changeNames <- function(data){
-  names(data)[names(data)=="Avg.CPC"] <- "CPC"
-  names(data)[names(data)=="Avg.position"] <- "Position"
+  names(data)[names(data)=="Avg.CPC"] <- "AverageCPC"
+  names(data)[names(data)=="Avg.position"] <- "AveragePosition"
   names(data)[names(data)=="Clickconversionrate"] <- "CVR"
-  names(data)[names(data)=="Convertedclicks"] <- "Conversions"
+  names(data)[names(data)=="Convertedclicks"] <- "ConvertedClicks"
   names(data)[names(data)=="Totalconv.value"] <- "ConversionValue"
-  names(data)[names(data)=="Cost/convertedclick"] <- "CPO"
+  names(data)[names(data)=="Cost/convertedclick"] <- "CostPerConvertedClick"
   names(data)[names(data)=="Value/convertedclick"] <- "ValuePerConversion"
   names(data)[names(data)=="Value/conv."] <- "ValuePerConversion"
   return(data)
